a4f05b699a939c9c876f431cb3adbbdcbcd1051d
David Erb <david.erb@diamond.ac.uk>
2023-01-30 13:59:01 +0000

    upload code

---
+++ .dae-devops/prepare_git_dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ********** Please don't edit this file!
+# ********** It has been generated automatically by dae_devops version 0.3.2.
+# ********** For repository_name dls-servbase
+
+me=${BASH_SOURCE}
+echo "${me}: installing 0 dependencies for repository_name dls-servbase"
+
+function __install {
+    echo ${me}: "$@"
+    "$@"
+}
+
+
+# dae_devops_fingerprint aa52a2cf33a446b394f673309435924a
