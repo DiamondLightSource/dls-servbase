4845f1e6e53b22635293fee14264190b84931383
David Erb <david.erb@diamond.ac.uk>
2023-02-21 08:55:07 +0000

    tweaks info to be debug log

--- src/dls_servbase_lib/base_aiohttp.py
+++ src/dls_servbase_lib/base_aiohttp.py
@@ -532,10 +532,10 @@ class BaseAiohttp:
         if self.__cookie_specification is None:
             return
 
-        logger.info("[RELCOOK] releasing cookies")
+        logger.debug("[RELCOOK] releasing cookies")
 
         for cookie in opaque.cookies.list():
-            logger.info("[RELCOOK] releasing cookie")
+            logger.debug("[RELCOOK] releasing cookie")
             await cookie.release()
 
     # ----------------------------------------------------------------------------------------
